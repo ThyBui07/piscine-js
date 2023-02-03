3df717c9bfca94441f6292b0d487d51827052005
ThyBui07 <buithinguyenthy@gmail.com>
2023-02-03 09:57:50 +0200

    bloody-sunday

--- bloody-sunday.js
+++ bloody-sunday.js
@@ -24,14 +24,3 @@ function millisecToDays(millisec) {
     return Math.floor(millisec / (1000 * 60 * 60 * 24))
 }
 
-// console.log(bloodySunday(new Date('0001-01-01'))) // 'Monday')
-// console.log(bloodySunday(new Date('0001-01-02'))) // 'Tuesday')
-// console.log(bloodySunday(new Date('0001-01-03'))) // 'Wednesday')
-// console.log(bloodySunday(new Date('0001-01-04'))) // 'Thursday')
-// console.log(bloodySunday(new Date('0001-01-05'))) // 'Friday')
-// console.log(bloodySunday(new Date('0001-01-06'))) // 'Saturday')
-// console.log(bloodySunday(new Date('0001-01-07'))) // 'Monday')
-// console.log(bloodySunday(new Date('0001-12-01'))) // 'Friday')
-// console.log(bloodySunday(new Date('1664-08-09'))) // 'Saturday')
-// console.log(bloodySunday(new Date('2020-01-01'))) // 'Monday')
-// console.log(bloodySunday(new Date('2048-12-08'))) // 'Thursday')
